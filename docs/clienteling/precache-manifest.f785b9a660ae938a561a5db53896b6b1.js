03cc8f263e9b05f69794622ab6cb1ebd04979fa7
Matthew Morey <me@matthewmorey.com>
2020-04-20 14:29:45 -0700

    Adding Jekyll for Github Pages.

---
+++ docs/clienteling/precache-manifest.f785b9a660ae938a561a5db53896b6b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "301020fa3f5a988f7fcf2a0507f6969d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "05f22f39632b7ffdf7a0",
+    "url": "./static/css/main.8d4f3a91.chunk.css"
+  },
+  {
+    "revision": "d5d3096ff67bcf584460",
+    "url": "./static/js/2.50a794ca.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "./static/js/2.50a794ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05f22f39632b7ffdf7a0",
+    "url": "./static/js/main.f32c5e81.chunk.js"
+  },
+  {
+    "revision": "7304c6cd1204852fb3fb",
+    "url": "./static/js/runtime-main.1451e3c5.js"
+  }
+]);
